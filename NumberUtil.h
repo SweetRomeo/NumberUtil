bfeec91a93b4bba8bb9efc21b1e0d4485b6a8754
Berke Sertel <76247074+SweetRomeo@users.noreply.github.com>
2023-04-25 00:07:59 +0300

    Add files via upload

---
+++ NumberUtil.h
@@ -0,0 +1,37 @@
+//
+// Created by BERKE on 14.03.2023.
+//
+
+#ifndef UNTITLED2_NUMBERUTIL_H
+#define UNTITLED2_NUMBERUTIL_H
+bool areFriends(int,int);
+int Mid(int,int,int);
+void printDiamond(int);
+void BallShapeTest();
+int calculateDigitalRoot(int);
+int calculateFactorial(int);
+double calculate_e_number();
+void displayCollatz(int n);
+int getScale1();
+int getScale2(int);
+int getScale3(int,int);
+bool lottoGame1();
+void lotto_game1_test(int);
+bool lottoGame2();
+void lotto_game2_test(int);
+bool lottoGame3();
+void lotto_game3_test(int);
+void printPrimeFactors(int);
+int getPrimeFactorsSum(int);
+bool isPerfect(int);
+bool isOkay(int);
+int getPrimeIndex(int);
+bool isArmstrong(int);
+bool isFactorian(int);
+bool isPrime(int);
+bool isPrimeX(int);
+bool isSuperPrime(int);
+int reverseNumber(int);
+void Operation();
+void Goldbach(int);
+#endif //UNTITLED2_NUMBERUTIL_H
